9eb64c58f280c05bbc570b9356ff9c9eb141d895
SoojinY <2018112174@dgu.ac.kr>
2020-09-23 16:15:10 +0900

    'readline'

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-command = input()
-command = command.split(" ")
+import sys
+command = sys.stdin.readline().split(" ")
 
 N = int(command[0])
 K = int(command[1])
@@ -8,19 +8,21 @@ josephus = []
 for i in range(N):
     josephus.append(0)
 
-index = 0
+index = 1
 count = 0
 order = []
 
 while 0 in josephus:
-    if josephus[index] < 1:
+    if josephus[index-1] < 1:
         if count < K-1:
             count += 1
         else:
-            josephus[index] = 1
+            josephus[index-1] = 1
             count = 0
-            order.append(index + 1)
+            order.append(index)
     index += 1
     index %= N
+    if index < 1:
+        index += N
 
 print(order)
