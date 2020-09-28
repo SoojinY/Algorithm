ef81feacc49424aff9c5a0d2e9c434aa96463077
SoojinY <2018112174@dgu.ac.kr>
2020-09-28 13:32:18 +0900

    'deque'

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 import sys
-command = sys.stdin.readline().split(" ")
+from collections import deque
 
-N = int(command[0])
-K = int(command[1])
+N, K = map(int, sys.stdin.readline().split(" "))
+dq = deque([i for i in range(1, N+1)])
+res = []
+while dq:
+    dq.rotate(-K+1)
+    res.append(str(dq.popleft()))
 
-josephus = []
-for i in range(N):
-    josephus.append(0)
-
-index = 1
-count = 0
-order = []
-
-while 0 in josephus:
-    if josephus[index-1] < 1:
-        if count < K-1:
-            count += 1
-        else:
-            josephus[index-1] = 1
-            count = 0
-            order.append(index)
-    index += 1
-    index %= N
-    if index < 1:
-        index += N
-
-print('<',end="")
-for i in range(len(order)):
-    print(order[i],end="")
-    if i<len(order)-1:
-        print(', ',end="")
-
-print('>')
+sys.stdout.write("<"+", ".join(res)+">")
